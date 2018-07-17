8f6240c8c7fdd4448ff8650b21473043fa379598
Elvis de Freitas <edigitalb@gmail.com>
2018-07-17 14:46:47 -0300

    should not stop spring start when kafka is offline

--- spring-kafka/src/main/java/com/mageddo/kafka/consumer/ConsumerDeclarer.java
+++ spring-kafka/src/main/java/com/mageddo/kafka/consumer/ConsumerDeclarer.java
@@ -1,22 +1,30 @@
 package com.mageddo.kafka.consumer;
 
 import com.mageddo.kafka.TopicDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.retry.RetryPolicy;
 import org.springframework.retry.backoff.BackOffPolicy;
 import org.springframework.retry.backoff.ExponentialBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.support.CronTrigger;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class ConsumerDeclarer {
+public class ConsumerDeclarer implements SchedulingConfigurer {
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 	private ConfigurableBeanFactory beanFactory;
+	private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
 	private KafkaProperties kafkaProperties;
 	private boolean autostartup;
 
@@ -24,6 +32,7 @@ public class ConsumerDeclarer {
 		this.beanFactory = beanFactory;
 		this.kafkaProperties = kafkaProperties;
 		this.autostartup = autostartup;
+		this.kafkaListenerEndpointRegistry = beanFactory.getBean(KafkaListenerEndpointRegistry.class);
 	}
 
 	public void declare(final TopicDefinition... topics) {
@@ -47,7 +56,9 @@ public class ConsumerDeclarer {
 		if(autoStartup){
 			factory.setConcurrency(topic.getConsumers());
 		}
-		factory.setAutoStartup(autoStartup);
+
+		// will startup using endpoint registry to prevent application startup fail when kafka is down
+		factory.setAutoStartup(false);
 //		factory.getContainerProperties().setAckOnError(false);
 		factory.getContainerProperties().setAckMode(topic.getAckMode());
 		factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(kafkaProperties.buildConsumerProperties()));
@@ -70,6 +81,20 @@ public class ConsumerDeclarer {
 			factory.setRetryTemplate(retryTemplate);
 		}
 		beanFactory.registerSingleton(topic.getFactory(), factory);
+	}
+
+	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+		if(!autostartup){
+			logger.warn("status=autoStartup-disabled");
+			return;
+		}
+		taskRegistrar.addTriggerTask(() -> {
+			try {
+				kafkaListenerEndpointRegistry.start();
+			} catch (Exception e){
+				logger.warn("status=consumer-declare-failed, msg={}", e.getMessage(), e);
+			}
+		}, new CronTrigger("0 0/1 * * * *"));
 	}
 
 	private RetryTemplate getRetryTemplate(BackOffPolicy policy, RetryPolicy retryPolicy) {
