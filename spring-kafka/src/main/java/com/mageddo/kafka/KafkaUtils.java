48f200460244614e3c96396b7772a375265b1256
Elvis de Freitas Souza <edigitalb@gmail.com>
2018-12-19 21:11:27 -0200

    converting to class

--- spring-kafka/src/main/java/com/mageddo/kafka/KafkaUtils.java
+++ spring-kafka/src/main/java/com/mageddo/kafka/KafkaUtils.java
@@ -4,12 +4,15 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.listener.adapter.RetryingMessageListenerAdapter;
 import org.springframework.retry.RetryContext;
 
-public interface KafkaUtils {
+public class KafkaUtils {
 
-	static String nextTopic(String topic){
+	private KafkaUtils() {}
+
+	public static String nextTopic(String topic){
 		return nextTopic(topic, 1);
 	}
-	static String nextTopic(String topic, int retriesTopics){
+
+	public static String nextTopic(String topic, int retriesTopics){
 		final int i = topic.toUpperCase().indexOf("_RETRY");
 		if(i >= 0){
 			return getDLQ(topic.substring(0, i));
@@ -17,11 +20,11 @@ public interface KafkaUtils {
 		return topic + "_RETRY";
 	}
 
-	static String getDLQ(String topic){
+	public static String getDLQ(String topic){
 		return String.format("%s_DLQ", topic);
 	}
 
-	static <K, V>ConsumerRecord<K, V> getRecord(RetryContext retryContext){
+	public static <K, V>ConsumerRecord<K, V> getRecord(RetryContext retryContext){
 		return (ConsumerRecord<K, V>)retryContext.getAttribute(RetryingMessageListenerAdapter.CONTEXT_RECORD);
 	}
 }
