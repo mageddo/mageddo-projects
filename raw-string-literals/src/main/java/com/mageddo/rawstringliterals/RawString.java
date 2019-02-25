ad9279731b12a3ba75325ae13cb9f033c7b86d37
Elvis de Freitas <edigitalb@gmail.com>
2019-02-25 04:26:49 -0300

    using java 11 compiler instead of java 8

--- raw-string-literals/src/main/java/com/mageddo/rawstringliterals/RawString.java
+++ raw-string-literals/src/main/java/com/mageddo/rawstringliterals/RawString.java
@@ -3,7 +3,7 @@ package com.mageddo.rawstringliterals;
 import java.lang.annotation.*;
 
 @Documented
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.LOCAL_VARIABLE, ElementType.TYPE, ElementType.FIELD})
 public @interface RawString {
 
