cea62aa172876dff3af006b55d204d0ea0518aef
Elvis de Freitas <edigitalb@gmail.com>
2019-02-25 23:41:09 -0300

    adjusting dependencies

--- raw-string-literals/src/main/java/com/mageddo/rawstringliterals/RawStrings.java
+++ raw-string-literals/src/main/java/com/mageddo/rawstringliterals/RawStrings.java
@@ -2,11 +2,12 @@ package com.mageddo.rawstringliterals;
 
 public final class RawStrings {
 	/**
-	 * This methods grants RSL will work otherwise a exception will be thrown
+	 * This method requires RSL to work otherwise an exception will be thrown
 	 *
 	 * @throws IllegalStateException if RSL didn't inject the variable
+	 * @return Throws an exception if called
 	 */
 	public static String lateInit(){
-		throw new IllegalStateException("raw string literals should inject this variable");
+		throw new IllegalStateException("RSL should inject this variable");
 	}
 }
